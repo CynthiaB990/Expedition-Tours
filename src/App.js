4bd34b8021b6460477e15f3795ca372e2e776f81
Cynthia Nakhumicha Barasa <cynthiabarasa312@gmail.com>
2023-05-02 22:33:31 +0300

    Imported components items to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react"
 import './App.css'
+import Navbar from './Components/NavBar/Navbar'
+import Home from './Components/Home/Home'
+import Main from './Components/Main/Main'
+import Footer from './Components/Footer/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
